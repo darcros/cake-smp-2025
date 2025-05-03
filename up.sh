5a6fb8b95bf8f3d11a24f5fee35624191d935e50
darcros <darcros7@gmail.com>
2025-05-03 17:33:59 +0200

    make stuff work with podman

--- up.sh
+++ up.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 USER=1000
 GROUP=1000
 
-mkdir -p ./data/smp/universe; chown $USER:$GROUP ./data/smp/universe
-mkdir -p ./data/smp/databases; chown $USER:$GROUP ./data/smp/databases
-mkdir -p ./data/smp/logs; chown $USER:$GROUP ./data/smp/logs
+mkdir -p ./data/smp/universe; podman unshare chown $USER:$GROUP ./data/smp/universe
+mkdir -p ./data/smp/databases; podman unshare chown $USER:$GROUP ./data/smp/databases
+mkdir -p ./data/smp/logs; podman unshare chown $USER:$GROUP ./data/smp/logs
 
-mkdir -p ./data/smp-backup/backups; chown $USER:$GROUP ./data/smp-backup/backups
+mkdir -p ./data/smp-backup/backups; podman unshare chown $USER:$GROUP ./data/smp-backup/backups
 
-docker compose up --build
+podman compose up --build
