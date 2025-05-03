82431c05fb35f3967db5d137d514483e239b30b8
darcros <darcros7@gmail.com>
2025-05-03 20:15:44 +0200

    update volumes

--- up.sh
+++ up.sh
@@ -4,7 +4,10 @@ set -euo pipefail
 USER=1000
 GROUP=1000
 
-mkdir -p ./data/smp/; podman unshare chown $USER:$GROUP ./data/smp
-mkdir -p ./data/smp-backup/backups; podman unshare chown $USER:$GROUP ./data/smp-backup/backups
+# ensure all mounted directories exist and are owned by the correct user
+mkdir -p "./data/smp/server/"
+mkdir -p "./data/smp/server/squaremap/web"
+mkdir -p "./data/smp-backup/backups"
+podman unshare chown -R "$USER:$GROUP" "./data"
 
 podman compose up --build
