b1aff3bf7362c29ccd1b517dc3dfe09d9e60d695
darcros <darcros7@gmail.com>
2025-05-29 23:05:49 +0200

    fix: update volume mount options and remove redundant directory creation in up.sh

--- up.sh
+++ up.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-USER=1000
-GROUP=1000
-
-# ensure all mounted directories exist and are owned by the correct user
-mkdir -p "./data/smp/server/"
-mkdir -p "./data/smp/server/squaremap/web"
-mkdir -p "./data/smp-backup/backups"
-podman unshare chown -R "$USER:$GROUP" "./data"
-
 podman compose up --build
